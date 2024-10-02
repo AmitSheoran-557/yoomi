8dcaabca9cfa46b1422fc26b119582eeaffcdc09
AmitSheoran-557 <amit22082006@gmail.com>
2024-10-02 11:53:25 +0530

    slider edit

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,6 +22,8 @@ $('.sec-3-slider').slick({
     variableWidth: true,
     slidesToShow: 1.2,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
     prevArrow: '.prevarrow',
     nextArrow: '.nextarrow',
     responsive: [
@@ -76,7 +78,7 @@ $(document).ready(function () {
         fade: true,
         asNavFor: ".second-row-slider",
         infinite: true,
-        
+
     });
 
     $(".second-row-slider").slick({
@@ -91,31 +93,31 @@ $(document).ready(function () {
 
         responsive: [
             {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                centerMode: true,
-                dots: false
-              },
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    centerMode: true,
+                    dots: false
+                },
             },
             {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                centerMode: true,
-                slidesToScroll: 1
-              }
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode: true,
+                    slidesToScroll: 1
+                }
             },
             {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                centerMode: true,
-                slidesToScroll: 1
-              },
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                    slidesToScroll: 1
+                },
             },
-          ]
+        ]
     });
 });
